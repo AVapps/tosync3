c7c0881767801777e6cd22b9f7b3d7850a5b7968
Adrien Vermeulen <adrien.vermeulen@free.fr>
2021-06-10 12:12:09 +0200

    Loading button component

---
+++ src/components/LoadingButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-button :disabled="loading">
+    <template v-if="loading">
+      <ion-spinner />
+    </template>
+    <template v-else>
+      <slot />
+    </template>
+  </ion-button>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { IonButton, IonSpinner } from '@ionic/vue'
+
+export default defineComponent({
+  name: 'LoadingButton',
+  components: { IonButton, IonSpinner },
+  props: { loading: Boolean },
+  watch: {
+    loading(newState) {
+      if (this.$el && newState) {
+        this.$el.style.minWidth = this.$el.offsetWidth + 'px'
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.p-button {
+  text-align: center;
+
+  > .loading-spinner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+  }
+}
+</style>
