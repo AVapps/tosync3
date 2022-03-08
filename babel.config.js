edab4be9654669a9aacc0a906fb7bb9603bf57d9
Adrien Vermeulen <adrien.vermeulen@free.fr>
2022-03-08 11:47:34 +0100

    Add babel regenerator plugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  "plugins": [
+    ["@babel/plugin-transform-runtime",
+      {
+        "regenerator": true
+      }
+    ]
   ]
 }
