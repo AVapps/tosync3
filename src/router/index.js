56faa38cef2292253891f840804bd4742f98061f
Adrien Vermeulen <adrien.vermeulen@free.fr>
2022-05-16 10:10:25 +0200

    Noms des routes et renommer les pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
-import Tabs from '@/views/Tabs.vue'
+import { until } from '@vueuse/core'
+import { useMainStore, useUser } from '@/store'
+import TabsPage from '@/views/TabsPage.vue'
 
 const routes = [
   {
     path: '/',
+    name: 'root',
     redirect: '/tabs/home'
   },
   {
     path: '/tabs/',
-    component: Tabs,
+    name: 'tabs',
+    component: TabsPage,
     children: [
       {
         path: '',
@@ -16,27 +20,55 @@ const routes = [
       },
       {
         path: 'home',
-        component: () => import('@/views/Home.vue')
+        name: 'home',
+        component: () => import('@/views/HomePage.vue')
       },
       {
         path: 'planning',
-        component: () => import('@/views/Planning.vue')
+        name: 'planning',
+        component: () => import('@/views/PlanningPage.vue')
       },
       {
         path: 'settings',
-        component: () => import('@/views/Settings.vue')
+        name: 'settings',
+        component: () => import('@/views/SettingsPage.vue')
       }
     ]
   },
   {
     path: '/welcome',
-    component: () => import('@/views/Welcome.vue')
+    name: 'welcome',
+    component: () => import('@/views/WelcomePage.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/UserSelectPage.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach(async (to) => {
+  if (to.path === '/welcome' || to.path === '/login') {
+    return
+  }
+
+  const main = useMainStore()
+  await until(() => main.isReady).toBe(true)
+  if (main.firstUse) {
+    return '/welcome'
+  }
+
+  const user = useUser()
+  await until(() => user.isReady).toBe(true)
+  if (!user.userId) {
+    return '/login'
+  }
+  
 })
 
 export default router
