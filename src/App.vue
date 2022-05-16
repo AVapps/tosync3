65dad9f5f863c3eeaac8fe4f44ddc6f678a13a82
Adrien Vermeulen <adrien.vermeulen@free.fr>
2022-05-16 10:03:35 +0200

    Simplification layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
   <ion-app>
-    <loading-page v-if="!user.isReady" />
-    <ion-router-outlet v-if="user.isReady && user.userId" />
-    <welcome-page v-if="user.isReady && !user.userId" />
+    <ion-router-outlet />
   </ion-app>
 </template>
 
 <script setup>
 import { IonApp, IonRouterOutlet } from '@ionic/vue'
-import LoadingPage from '@/views/LoadingPage.vue'
-import WelcomePage from './views/Welcome.vue'
 import { watchEffect } from 'vue'
-import { useMainStore, useUser, useCrews } from '@/store'
-import { useRouter } from 'vue-router'
+import { useMainStore, useUser, useCrews, useConnect } from '@/store'
 
 const store = useMainStore()
 const user = useUser()
 const crews = useCrews()
-const router = useRouter()
+const connect = useConnect()
 
 window.user = user
 window.store = store
 window.crews = crews
+window.connect = connect
 
 watchEffect(() => {
   document.firstElementChild.setAttribute('data-theme', store.config.theme)
