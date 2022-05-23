c629ae69fa0aed6c9e20e6c694c43eb754aaed4d
Adrien Vermeulen <adrien.vermeulen@free.fr>
2022-05-23 10:55:12 +0200

    Co ntrollable Modal component

---
+++ src/components/ControllableModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-modal
+    :is-open="modalOpenRef"
+    @didDismiss="closeModal()"
+  >
+    <ion-page>
+      <slot></slot>
+    </ion-page>
+  </ion-modal>
+</template>
+
+<script setup>
+// eslint-disable-next-line no-undef
+const modalOpenRef = ref(false)
+
+function openModal() {
+  modalOpenRef.value = true
+}
+
+function closeModal() {
+  modalOpenRef.value = false
+}
+
+// eslint-disable-next-line no-undef
+defineExpose({
+  openModal,
+  closeModal
+})
+
+</script>
+
+<style lang="scss" scoped>
+</style>
