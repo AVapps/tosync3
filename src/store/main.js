3627e39d34a8f53460708e4a39c8f4be3f14bb65
Adrien Vermeulen <adrien.vermeulen@free.fr>
2022-05-23 11:00:45 +0200

    Simplification config + prompt serverUrl

--- src/store/main.js
+++ src/store/main.js
@@ -1,24 +1,30 @@
 import { defineStore } from 'pinia'
 import { computed, readonly, toRef, watch } from 'vue'
-import { useAsyncState, useStorage, until, invoke } from '@vueuse/core'
+import { useAsyncState, useStorage, until, invoke, pausableWatch } from '@vueuse/core'
 import { Network } from '@capacitor/network'
 import { DateTime } from 'luxon'
 import { Storage } from '@capacitor/storage'
 import { useCapacitorStorageState } from '@/lib/useCapacitorStorage'
 import { useConnect, useUser, useCrews, usePlanning } from './index.js'
 import { importCrewConnectPlanning } from '@/lib/CrewConnect/importCCPlanning.js'
+import { toastHttpError } from '@/helpers/toast.js'
+import { useServerUrlPrompt } from '@/helpers/alert.js'
 
 const GLOBAL_CONFIG_KEY = 'TOSYNC._GLOBAL_.config'
 const LAST_CREWS_INDEX_SYNC_KEY = 'TOSYNC._GLOBAL_.lastCrewsIndexSync'
-const FIRST_USE_KEY = 'TOSYNC._GLOBAL_.firstUse'
 
 const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
 const GLOBAL_CONFIG_DEFAULTS = {
-  theme: prefersDark.matches ? 'dark' : 'light'
+  theme: prefersDark.matches ? 'dark' : 'light',
+  autoConnect: false,
+  firstUse: true
 }
 const NETWORK_STATUS_DEFAULTS = { connecte: false, connectionType: 'unknown' }
 
 export const useMainStore = defineStore('main', () => {
+  // state
+  const errorRef = ref()
+
   // sub stores
   const connect = useConnect()
   const crews = useCrews()
@@ -28,7 +34,6 @@ export const useMainStore = defineStore('main', () => {
   // Global persistent config
   const { state: config, isReady: configIsReady } = useCapacitorStorageState(GLOBAL_CONFIG_KEY, GLOBAL_CONFIG_DEFAULTS)
   const lastCrewsIndexSync = useStorage(LAST_CREWS_INDEX_SYNC_KEY, '')
-  const { state: firstUse, isReady: firstUseIsReady } = useCapacitorStorageState(FIRST_USE_KEY, true)
   
   // Application network state : { connected: boolean, connectionType: string 'wifi' | 'cellular' | 'none' | 'unknown' }
   const { state: networkStatus, isReady: networkStatusIsReady } = useAsyncState(Network.getStatus(), NETWORK_STATUS_DEFAULTS)
@@ -38,29 +43,58 @@ export const useMainStore = defineStore('main', () => {
   })
 
   // actions
+  const promptForServerUrl = useServerUrlPrompt()
   async function signIn({ silent = false }) {
+    pauseAutoLogin()
     console.log('main.signIn', silent)
-    const userId = await connect.signIn(user.userId, { silent })
-    if (userId) {
-      if (userId !== user.userId) {
-        await until(() => crews.isReady).toBe(true)
-        const userProfile = crews.get(userId)
-        if (!userProfile) {
-          throw new Error(`Profil utilisateur « ${userId} » introuvable !`)
-        }
-        if (userProfile.photoThumbnail) {
-          userProfile._attachments = {
-            'photo.jpg': {
-              content_type: 'image/jpg',
-              data: await connect.getCrewPhoto(userProfile.photoThumbnail, { format: 'blob' })
+
+    if (!connect.serverUrl) {
+      if (silent) {
+        resumeAutoLogin()
+        return
+      }
+      const { role, data } = await promptForServerUrl({
+        header: 'Adresse du serveur',
+        message: 'Entrez l\'adresse du serveur CrewConnect. Une fois renseignée, celle-ci peut être modifiée dans les réglages.',
+        value: '',
+        placeholder: 'https://adresse.du.serveur'
+      })
+      if (role === 'confirm' && data?.validatedUrl) {
+        connect.serverUrl = data.validatedUrl
+      } else {
+        resumeAutoLogin()
+        return
+      }
+    }
+    
+    try {
+      const userId = await connect.signIn(user.userId, { silent })
+      if (userId) {
+        if (userId !== user.userId) {
+          await until(() => crews.isReady).toBe(true)
+          const userProfile = crews.get(userId)
+          if (!userProfile) {
+            throw new Error(`Profil utilisateur « ${userId} » introuvable !`)
+          }
+          if (userProfile.photoThumbnail) {
+            userProfile._attachments = {
+              'photo.jpg': {
+                content_type: 'image/jpg',
+                data: await connect.getCrewPhoto(userProfile.photoThumbnail, { format: 'blob' })
+              }
             }
           }
+          
+          await user.addUser(userProfile)
+          user.setUser(userProfile)
         }
-        
-        await user.addUser(userProfile)
-        user.setUser(userProfile)
+        await onCrewConnectLogin()
       }
-      await onCrewConnectLogin()
+    } catch (error) {
+      toastHttpError(error)
+      errorRef.value = error
+    } finally {
+      resumeAutoLogin()
     }
   }
 
@@ -73,7 +107,7 @@ export const useMainStore = defineStore('main', () => {
     console.log('main.toggleTheme', config.value)
   }
 
-  // Watchers
+  // Update crews index when needed
   invoke(async () => {
     await until(() => crews.isReady && connect.isConnected && networkStatus.value.connected).toBe(true)
     console.log('Connected and ready : loading crews')
@@ -83,33 +117,26 @@ export const useMainStore = defineStore('main', () => {
     console.log('crews.lastCrewIndexSync', lastCrewsIndexSync.value, oneWeekAgo)
 
     if (lastCrewsIndexSync.value <= oneWeekAgo) {
-      try {
-        const crewsList = await connect.getCrewsIndex()
-        console.log('main.syncCrewsIndex', crewsList)
-        await crews.importList(crewsList, { overwriteTitles: true })
-        lastCrewsIndexSync.value = DateTime.utc().toISO()
-      } catch (error) {
-        console.error('main.syncCrewsIndex error', error)
-      }
+      await syncCrewsIndex(false)
     }
   })
 
-  watch(() => networkStatus.value.connected && user.userId && connect.serverUrl, async ready => {
-    if (ready) {
-      try {
+  // Watchers
+  const { pause: pauseAutoLogin, resume: resumeAutoLogin } = pausableWatch(
+    () => configIsReady.value && networkStatus.value.connected && user.userId && connect.serverUrl,
+    async ready => {
+      if (ready && config.value.autoConnect) {
         await signIn({ silent: true })
-      } catch (e) {
-        console.log('main.tryAutoRelogin error', e)
       }
     }
-  })
+  )
 
   // helpers
   async function onCrewConnectLogin() {
     console.log('main.onCrewConnectLogin')
-    const oneMinuteAgo = DateTime.utc().minus({ seconds: 60 }).toISO()
+    const fiveMinutesAgo = DateTime.utc().minus({ minutes: 5 }).toISO()
     await until(() => user.isReady).toBe(true)
-    if (user.config.lastPlanningSync <= oneMinuteAgo) {
+    if (user.config.lastPlanningSync <= fiveMinutesAgo) {
       const { success, results } = await syncPlanning()
       if (success) {
         console.log('main.syncPlanning success', results)
@@ -120,31 +147,64 @@ export const useMainStore = defineStore('main', () => {
     }
   }
 
+  async function syncCrewsIndex(toastOnError = true) {
+    try {
+      const crewsList = await connect.getCrewsIndex()
+      console.log('main.syncCrewsIndex', crewsList)
+      await crews.importList(crewsList, { overwriteTitles: true })
+      lastCrewsIndexSync.value = DateTime.utc().toISO()
+    } catch (error) {
+      console.log('main.syncCrewsIndex error', error)
+      errorRef.value = error
+      if (toastOnError) {
+        toastHttpError(error)
+      }
+    }
+  }
+
   async function syncPlanning() {
-    const data = await connect.getLatestRosterCalendars(user.config.lastPlanningSync)
-    const result = await planning.importCrewConnectData(data)
+    const rosterData = await connect.getLatestRosterCalendars(user.config.lastPlanningSync)
+    const result = await planning.importCrewConnectData(rosterData)
+    console.log(result)
+    return result
+  }
+
+  async function syncPlanningInterval({ dateFrom, dateTo }) {
+    const rosterData = await connect.getRosterCalendars({ dateFrom, dateTo })
+    const result = await planning.importCrewConnectData(rosterData)
+    console.log(result)
     return result
   }
 
   async function signOut() {
-    if (connect.isConnected) {
-      await connect.signOut()
+    pauseAutoLogin()
+    try {
+      if (connect.isConnected) {
+        await connect.signOut()
+      }
+      user.logout()
+    } catch (error) {
+      errorRef.value = error
+      toastHttpError(error)
+    } finally {
+      resumeAutoLogin()
     }
-    user.logout()
   }
 
 
   return {
     config,
+    error: errorRef,
     network: readonly(networkStatus),
     lastCrewsIndexSync: readonly(lastCrewsIndexSync),
-    firstUse,
     lastPlanningSync: computed(() => user.config.lastPlanningSync),
     isDark: computed(() => config.value.theme === 'dark'),
     toggleTheme,
     signIn,
     signOut,
     syncPlanning,
-    isReady: computed(() => configIsReady.value && networkStatusIsReady.value && firstUseIsReady.value)
+    syncPlanningInterval,
+    syncCrewsIndex,
+    isReady: computed(() => configIsReady.value && networkStatusIsReady.value)
   }
 })
